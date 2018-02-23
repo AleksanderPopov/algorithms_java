ff74d30bfcd6f9bbfee649acf0415ed8753f412d
Alexander Popov <aleksander.popov.93@gmail.com>
2018-02-23 22:12:46 +0200

    BFS fixes

--- src/main/java/algorithms/datastructures/graph/BFS.java
+++ src/main/java/algorithms/datastructures/graph/BFS.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 
 public class BFS {
     public static void perform(Graph graph, int v) {
-        Pair<Boolean, Integer>[] table = new Pair[graph.V()];
+        Pair[] table = new Pair[graph.V()];
         perform(graph, table, v);
 
         String tableToString = IntStream.range(0, table.length)
@@ -14,12 +14,14 @@ public class BFS {
                 .reduce((f, s) -> String.format("%s\n%s", f, s))
                 .orElseThrow(RuntimeException::new);
 
+        System.out.println("------");
         System.out.println("BFS:");
         System.out.println(tableToString);
+        System.out.println("------");
     }
 
-    private static void perform(Graph graph, Pair<Boolean, Integer>[] table, Integer v) {
-        table[v] = new Pair<>(true, null);
+    private static void perform(Graph graph, Pair[] table, Integer v) {
+        table[v] = new Pair(true, null);
 
         Queue<Integer> queue = new ArrayDeque<>(graph.V());
         queue.add(v);
@@ -29,17 +31,17 @@ public class BFS {
             graph.adj(cv).forEach(adj -> {
                 if (table[adj] == null || !table[adj].v1) {
                     queue.add(adj);
-                    table[adj] = new Pair<>(true, cv);
+                    table[adj] = new Pair(true, cv);
                 }
             });
         }
     }
 
-    static class Pair<K, V> {
-        public final K v1;
-        public final V v2;
+    static class Pair {
+        final Boolean v1;
+        final Integer v2;
 
-        public Pair(K v1, V v2) {
+        Pair(Boolean v1, Integer v2) {
             this.v1 = v1;
             this.v2 = v2;
         }
